212464e57f3989cb0c57b3aa6952663cc9478d4c
Eric Dürr <eric.durr.20@ull.edu.com>
2022-04-06 19:14:14 +0100

    initialized repo

---
+++ test/default.test.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import defaultFunction from '../src/default';
+
+describe('', () => {
+  it('', () => {
+    expect(defaultFunction()).to.be.true;
+  });
+});
