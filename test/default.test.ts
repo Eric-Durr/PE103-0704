f383e3c42f008bb02c2ca8f7ab24108aa0ebd4fb
Eric DS <eric.d.s.info@gmail.com>
2022-04-06 19:50:39 +0100

    add random class to test WebStorm integration

--- test/default.test.ts
+++ test/default.test.ts
@@ -2,8 +2,14 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import defaultFunction from '../src/default';
 
-describe('', () => {
+import { RandomClass } from '../src/randomClass.class';
+
+describe('Default test', () => {
   it('', () => {
     expect(defaultFunction()).to.be.true;
+  });
+  it('Testing random class', () => {
+    const obj: RandomClass = new RandomClass('Eric');
+    expect(obj.name).to.be.eq('Eric');
   });
 });
