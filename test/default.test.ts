27d3aef8b59f2b0654b411e81559be439d1ebc30
Eric Dürr <eric.durr.20@ull.edu.com>
2022-04-28 00:11:26 +0100

    bruh

--- test/default.test.ts
+++ test/default.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import defaultFunction from '../src/default';
+import trueFunction from '../src/index';
 
 describe('', () => {
   it('', () => {
